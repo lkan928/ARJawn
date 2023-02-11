545ef8f80974b95d522eb0868d122b911c68326f
TuStep <tuk83148@temple.edu>
2023-02-11 11:38:45 -0500

    Start of AR Jawn

---
+++ AR Jawn/AR JawnTests/AR_JawnTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_JawnTests.swift
+//  AR JawnTests
+//
+//  Created by Timmy Tu on 2/11/23.
+//  Copyright © 2023 Timmy Tu. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Jawn
+
+class AR_JawnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
